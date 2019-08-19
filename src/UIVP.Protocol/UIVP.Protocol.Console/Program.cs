f47c7e723bd41b8a3a73e736c187529e30961ffc
Felandil <s.fuhrhop@hotmail.de>
2019-08-19 11:23:44 +0200

    create nuget

--- src/UIVP.Protocol/UIVP.Protocol.Console/Program.cs
+++ src/UIVP.Protocol/UIVP.Protocol.Console/Program.cs
@@ -6,12 +6,15 @@ namespace UIVP.Protocol.Console
   using System.Collections.Generic;
   using System.Configuration;
 
+  using RestSharp;
+
   using Tangle.Net.Entity;
   using Tangle.Net.ProofOfWork;
   using Tangle.Net.Repository;
   using Tangle.Net.Repository.Client;
 
   using UIVP.Protocol.Core.Iota.Repository;
+  using UIVP.Protocol.Core.Repository;
   using UIVP.Protocol.Core.Services;
 
   class Program
@@ -24,12 +27,8 @@ namespace UIVP.Protocol.Console
 
     private static async void ExecuteAsync()
     {
-      // should be stored somewhere safe in production and loaded on demand
-      var cngKey = Encryption.CreateSignatureScheme();
-
       // register key in memory, just for testing. in production this will be the KvKPublicKeyRepository
-      var inMemoryPublicKeyRepository = new InMemoryPublicKeyRepository();
-      await inMemoryPublicKeyRepository.RegisterCompanyPublicKeyAsync("401196200", cngKey.Key);
+      var publicKeyRepository = new KvkPublicKeyRepository(new RestClient(ConfigurationManager.AppSettings["KvKApiUri"]));
 
       // in production, create instances via DI
       var imageService = new ImageInvoiceParser(ConfigurationManager.AppSettings["ApiUri"], ConfigurationManager.AppSettings["SubscriptionKey"]);
@@ -37,7 +36,11 @@ namespace UIVP.Protocol.Console
         new RestIotaRepository(
           new FallbackIotaClient(new List<string> { "https://nodes.devnet.thetangle.org:443" }, 5000),
           new PoWService(new CpuPearlDiver())),
-        inMemoryPublicKeyRepository);
+        publicKeyRepository);
+
+      // should be stored somewhere safe in production and loaded on demand
+      var cngKey = Encryption.CreateSignatureScheme();
+      await publicKeyRepository.RegisterCompanyPublicKeyAsync("401196200", cngKey.Key);
 
       // parse invoice data with the help of azure
       var invoice = await imageService.ParseInvoiceAsync("..\\..\\..\\IMG_20190413_192812.jpg");
